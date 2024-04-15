b90039d6ec8879a0876df36bf5b03bd3002df188
yokai <yokai@kali>
2024-04-15 11:30:51 -0400

    first commit

---
+++ fuzzerPro.py
@@ -0,0 +1,17 @@
+import requests
+import sys
+
+
+def loop():
+    for word in sys.stdin:
+        res = requests.get(url=f"http://10.10.212.140/{word}")
+        if res.status_code == 404:
+            loop()
+        else:
+            data = res.json()
+            print(data)
+            print(res.status_code)
+            print(word)
+        # data = res.json()
+        # print(data)
+loop()
